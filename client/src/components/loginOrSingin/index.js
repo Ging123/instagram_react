df0bdbd00ef30c6e2f16d608e55f5912dd2f379b
Ging123 <iceglauber@outlook.com>
2021-05-19 11:51:20 -0300

    create main container from login screen

---
+++ client/src/components/loginOrSingin/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../../global_css/containers.scss';
+import appStoreImg from '../../assets/app-store.png';
+import googlePlayImg from '../../assets/google-play.png';
+
+const altMensage = "it wasn't possible to load this image";
+
+const Or = ({className}) => {
+  return (
+    <div className={`or-and-line-container ${className}`}>
+      <div className="line"/>
+      <span>OR</span>
+      <div className="line"/>
+    </div>
+  )
+}
+
+Or.defaultProps = { className:"" }
+
+
+const GetTheApp = ({className}) => {
+  const linkToAppStore = "https://apps.apple.com/app/instagram/id389801252?vt=lo";
+  const linkToGooglePlay = "https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3D3831F102-0B1F-465C-8CA3-E8850EEF410A%26utm_content%3Dlo%26utm_medium%3Dbadge";
+
+  return (
+    <div className={`get-the-app-container ${className}`}>
+      <div className="text-align-center margin-bottom">Get the app.</div>
+      <div className="text-align-center">
+        <a href={linkToAppStore}>
+          <img src={appStoreImg} alt={altMensage} className="store-image"/>
+        </a>
+        <a href={linkToGooglePlay}>
+          <img src={googlePlayImg} alt={altMensage} className="store-image"/>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+GetTheApp.defaultProps = { className:"" }
+
+export {Or, GetTheApp};
--- client/src/pages/loginOrSingin/index.js
+++ client/src/pages/loginOrSingin/index.js
@@ -1,50 +1,55 @@
 import React, {useState} from 'react';
 import './style.scss';
 import logo from '../../assets/Instagram-Logo.png'
+import {Or, GetTheApp} from '../../components/loginOrSingin/index'; 
 
 const LoginOrSingin = () => {
   document.title = "Instagram";
   const altMensage = "it wasn't possible to load this image";
-
   const [nameOrNumber, setNameOrNumber] = useState("");
   const [password, setPassword] = useState("");
 
   return (
-    <section id="login-or-singin-container">
-      <div id="cellphone-image-container">
+    <section id="page-container">
+      <div id="cellphone-container">
         
       </div>
 
-      <div className="main-page-container">
+      <div className="login-or-singin-container">
         <div className="container">
           <img src={logo} alt={altMensage} className="instagram-logo"/>
-          <form className="flex-column">
+          <form className="flex-column small-margin-bottom">
             <input 
               type="text"
               value={nameOrNumber}
               onChange={(e) => setNameOrNumber(e.target.value)}
-              className="default-input"
+              className="default-input smaller-margin-bottom"
             />
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="default-input"
+              className="default-input small-margin-bottom"
             />
             <input
               type="submit"
               value="Log in"
+              className="main-button"
             />
           </form>
+          <Or/>
+          <div id="login-with-facebook-container">
+            <i className="fab fa-facebook-square facebook-icone"/>
+            <span id="login-with-facebook"> Log in with Facebook</span>
+          </div>
+          <div id="forgot-password">forgot password?</div>
         </div>
 
         <div className="container">
           <span id="singup-text">Don't have an account?</span>
         </div>
 
-        <div>
-          
-        </div>
+        <GetTheApp/>
       </div>
     </section>
   )
